ccbe6ac117b504c97bf43c9fa262e71e55b72281
sierradurham28842sierradurham <ZuckermanWedin1882@outlook.com>
2025-12-05 00:39:24 +0800

    add vite.config.ts, handler-module-module.go, helpers-resolver-product.go

---
+++ frontend/web/src/handler-module-module.go
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\frontend\web\src\handler-module-module.go
+// Generated at: 2025-10-17T06:25:37.726Z
---
+++ src-sms/spec-environment/helpers-resolver-product.go
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\src-sms\spec-environment\helpers-resolver-product.go
+// Generated at: 2025-10-17T06:25:37.723Z
